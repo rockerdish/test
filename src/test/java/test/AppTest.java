6137fa7afecc23ae68701640d4e0a1cc887f7cb7
Surender Singh Rathore <surenbika@gmail.com>
2017-09-19 17:11:18 +0530

    initial commit

---
+++ src/test/java/test/AppTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
